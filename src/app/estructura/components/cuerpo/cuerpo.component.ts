946da47d38c88e9164e9a7253a3d9247a1a604ed
Java <fjvalls@gmail.com>
2023-06-09 10:20:17 +0200

    Primeras modificaciones. Tabla

--- src/app/estructura/components/cuerpo/cuerpo.component.ts
+++ src/app/estructura/components/cuerpo/cuerpo.component.ts
@@ -12,10 +12,11 @@ export class CuerpoComponent {
     this.listadoHeroes = [
       {
         nombre: "Superman",
-        imagen: "",
+        imagen: "assets/img/avatars/superman.jpg",
         ataque: 80,
         defensa: 93,
         poder: 350,
+        honor: 94,
         vida: 100
       },
       {
@@ -23,7 +24,26 @@ export class CuerpoComponent {
         imagen: "",
         ataque: 60,
         defensa: 75,
-        poder: 100,
+        poder: 50,
+        honor: 75,
+        vida: 100
+      },
+      {
+        nombre: "Capitana Marvel",
+        imagen: "",
+        ataque: 79,
+        defensa: 80,
+        poder: 300,
+        honor: 70,
+        vida: 100
+      },
+      {
+        nombre: "Catwoman",
+        imagen: "",
+        ataque: 63,
+        defensa: 72,
+        poder: 50,
+        honor: -5,
         vida: 100
       },
       {
@@ -32,6 +52,25 @@ export class CuerpoComponent {
         ataque: 86,
         defensa: 89,
         poder: 150,
+        honor: 85,
+        vida: 100
+      },
+      {
+        nombre: "Viuda Negra",
+        imagen: "",
+        ataque: 88,
+        defensa: 83,
+        poder: 35,
+        honor: 37,
+        vida: 100
+      },
+      {
+        nombre: "Thanos",
+        imagen: "",
+        ataque: 90,
+        defensa: 95,
+        poder: 250,
+        honor: -92,
         vida: 100
       },
       {
@@ -40,6 +79,7 @@ export class CuerpoComponent {
         ataque: 82,
         defensa: 83,
         poder: 200,
+        honor: 80,
         vida: 100
       },
       {
@@ -48,6 +88,7 @@ export class CuerpoComponent {
         ataque: 83,
         defensa: 85,
         poder: 150,
+        honor: 69,
         vida: 100
       },
       {
@@ -56,6 +97,7 @@ export class CuerpoComponent {
         ataque: 64,
         defensa: 95,
         poder: 135,
+        honor: 95,
         vida: 100
       },
       {
@@ -64,8 +106,9 @@ export class CuerpoComponent {
         ataque: 92,
         defensa: 75,
         poder: 275,
+        honor: 60,
         vida: 100
-      }
+      },
     ];
   }
 }
